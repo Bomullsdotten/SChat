82890d2ab401b69add8bad4f77ac7b4252dd5d3d
Sondre <sondre.erstad@gmail.com>
2017-08-22 12:51:25 +0200

    separated html and css

---
+++ public/js/page.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Snicx on 22/08/2017.
+ */
+$(function () {
+    var socket = io();
+    $('form').submit(function () {
+        socket.emit('message', $('#m').val());
+        var text = $('#username').html() + ': '+ $('#m').val();
+        $('#messages').append($('<li class="user">').text(text));
+        $('#m').val('');
+        return false;
+    });
+
+    socket.on('message', function(message){
+        $('#messages').append($('<li>').text(message));
+    });
+
+    socket.on('user update', function (message) {
+        $('#messages').append($('<li class="connection">').text(message));
+    });
+
+    socket.on('username', function (username) {
+        $('#username').text(username);
+    });
+
+    socket.on('update userbase', function (a_list) {
+        $('#userbase').empty();
+        $('#userbase').append($('<li>').text('Current users in chat:'));
+        for (var i = 0; i < a_list.length; i++){
+            $('#userbase').append($('<li>').text(a_list[i]));
+        }
+    })
+});
