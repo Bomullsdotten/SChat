a3bf9f08d9b000e32ee0cf3c8843a213959b6e3c
Sondre <sondre.erstad@gmail.com>
2017-08-22 12:57:42 +0200

    fixed name generation bug

--- users.js
+++ users.js
@@ -4,7 +4,7 @@
 var name_list = [
     'Charlie Brown',
     'Patty',
-    'Snoopy',
+    'Snoopy']/*,
     'Violet Gray',
     'Schroeder',
     'Lucy van Pelt',
@@ -74,14 +74,14 @@ var name_list = [
     'Jesse James',
     'Buffalo Bill',
     'Ma Dalton'
-]
+]*/
 
 
 module.exports = {
     generateUsername: function (users) {
         // We wat to make fewer guesses if on unique usernames if the userbase is large
         var new_username = find_carton_name(users);
-        if (uniqueName(new_username, users)){
+        if (uniqueName(new_username, users) && new_username){
             return new_username;
         }
         return uniquely_modified_name(new_username, users)
